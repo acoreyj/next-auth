e56cd3dd68769d613d7e6173ce0800b5cea5770d
Corey Jepperson <corey@entropy.cc>
2023-01-26 10:08:51 -0600

    start

--- apps/examples/sveltekit/src/hooks.server.ts
+++ apps/examples/sveltekit/src/hooks.server.ts
@@ -1,7 +1,25 @@
 import { SvelteKitAuth } from "@auth/sveltekit"
 import GitHub from "@auth/core/providers/github"
-import { GITHUB_ID, GITHUB_SECRET } from "$env/static/private"
+import {
+  GITHUB_ID,
+  GITHUB_SECRET,
+  EMAIL_SERVER,
+  EMAIL_FROM,
+} from "$env/static/private"
+import { Email } from "@auth/core/providers/email"
 
 export const handle = SvelteKitAuth({
-  providers: [GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET })],
+  providers: [
+    GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET }),
+    Email({
+      id: "email",
+      name: "Email Magic Link",
+      server: EMAIL_SERVER as string,
+      from: EMAIL_FROM as string,
+      type: "email",
+      async sendVerificationRequest(params) {
+        console.log("params", params)
+      },
+    }),
+  ],
 })
